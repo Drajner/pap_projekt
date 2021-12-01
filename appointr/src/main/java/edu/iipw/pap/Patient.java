543242e8c6fc3d2eba5e90696847df9e700860ab
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2021-12-01 21:48:51 +0100

    Add docstrings to Patient.java

--- appointr/src/main/java/edu/iipw/pap/Patient.java
+++ appointr/src/main/java/edu/iipw/pap/Patient.java
@@ -2,22 +2,43 @@ package edu.iipw.pap;
 
 import java.time.LocalDate;
 
+/** A class representing a patient. */
 public class Patient extends Person{
+    /** The patient's reason for an appointment. */
     private String description;
 
+    /**
+     * Constructor for the Patient class.
+     * @param name The patient's first name.
+     * @param surname The patient's surname.
+     * @param birthDate The patient's birth date.
+     * @param description The patient's reason for an appointment.
+     */
     public Patient(String name, String surname, LocalDate dateOfBirth, String description) {
         super(name, surname, dateOfBirth);
         this.description = description;
     }
 
+    /**
+     * Returns the patient's reason for an appointment.
+     * @return The doctor's reason for an appointment.
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the patient's reason for an appointment.
+     * @param description The patient's reason for an appointment.
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Returns a string representation of the patient.
+     * @return A string representation of the patient.
+     */
     @Override
     public String toString() {
         return "Patient{" +
