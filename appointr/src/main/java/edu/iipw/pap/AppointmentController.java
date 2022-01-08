72f4392d9d2bef122c9a36963c51491d9a01d781
Jan Rusak <01158887@pw.edu.pl>
2022-01-08 22:44:59 +0100

    add functions for adding an appointment

--- appointr/src/main/java/edu/iipw/pap/AppointmentController.java
+++ appointr/src/main/java/edu/iipw/pap/AppointmentController.java
@@ -1,36 +1,58 @@
 package edu.iipw.pap;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class AppointmentController {
+    private Doctor doctor;
+    @FXML
+    private ChoiceBox<Patient> patientList;
+    @FXML
+    private DatePicker dateList;
+    @FXML
+    private Spinner<LocalTime> hourSelection;
+    @FXML
+    private TextField address;
+
+    public void cancel() {
+
+    }
 
     public Patient getPatient() {
-
+        return patientList.getValue();
     }
 
-    public String getDate() {
-
+    public LocalDate getDate() {
+        return dateList.getValue();
     }
 
-    public LocalDateTime getTime() {
+    public LocalTime getTime() {
+        return hourSelection.getValue();
+    }
 
+    public String getAddress() {
+        return address.getText();
     }
 
     public Appointment createAppointment() {
+        Patient patient = getPatient();
+        LocalDate date = getDate();
+        LocalTime time = getTime();
+        LocalDateTime dateTime = LocalDateTime.of(date, time);
+        String address = getAddress();
 
+        return new Appointment(doctor, patient, dateTime, address);
     }
 
     public void addAppointment() {
-
+        // doctor = moze LoggerController bedzie posiadal funckje ktora zwroci aktualnie zalogowanego lekarza
+        Appointment appointment = createAppointment();
+        // dodanie wizyty do bazy danych
     }
 
     public void editAppointment() {
