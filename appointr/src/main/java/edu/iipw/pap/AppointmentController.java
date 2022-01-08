7663d0f286fa56bf8c845b001f9252800e661d3b
Jan Rusak <01158887@pw.edu.pl>
2022-01-08 22:08:59 +0100

    create empty AppointmentController

---
+++ appointr/src/main/java/edu/iipw/pap/AppointmentController.java
@@ -0,0 +1,43 @@
+package edu.iipw.pap;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class AppointmentController {
+
+    public Patient getPatient() {
+
+    }
+
+    public String getDate() {
+
+    }
+
+    public LocalDateTime getTime() {
+
+    }
+
+    public Appointment createAppointment() {
+
+    }
+
+    public void addAppointment() {
+
+    }
+
+    public void editAppointment() {
+
+    }
+
+    public void deleteAppointment() {
+
+    }
+}
