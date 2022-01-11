4cfd260335ad425306cc4ac70ef45fcc279604bf
Jan Rusak <01158887@pw.edu.pl>
2022-01-11 23:56:41 +0100

    add patients to checkbox in DeletePatientController

--- appointr/src/main/java/edu/iipw/pap/DeletePatientController.java
+++ appointr/src/main/java/edu/iipw/pap/DeletePatientController.java
@@ -2,20 +2,34 @@ package edu.iipw.pap;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
-public class DeletePatientController {
+public class DeletePatientController implements Initializable {
 
-    public DeletePatientController() { }
+    public DeletePatientController() {}
 
+    @FXML
+    private ChoiceBox<String> patientList;
     @FXML
     private Button deleteButton;
     @FXML
     private Button cancelButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Populate populate = new Populate();
+        for (Patient p: populate.patients) {
+            patientList.getItems().add(p.toString());
+        }
+    }
 
     public void deletePatient(ActionEvent event) throws IOException {
         deletePatientFromData();
