84780ef8f7ea2f7d9d60ca0ff57ec7a09b3a10a4
Jakub Mateusz Marcowski <01158831@pw.edu.pl>
2022-01-18 22:38:32 +0100

    Create an executable of the project

---
+++ appointr/src/main/java/edu/iipw/pap/Main.java
@@ -0,0 +1,8 @@
+package edu.iipw.pap;
+
+/* This is done so that we can successfully export our JavaFX app into a .jar file */
+public class Main {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
